e0b3b68a99864a8e6d534fd0bd105bcdb2550e19
magda997202 <magda997202@my.yosemite.edu>
2025-12-06 11:22:21 -0800

    Completed Program

---
+++ build/CMakeFiles/BookStore.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/User/OneDrive/Documents/School/CSCI 271-3024/BookStore-main/Book.cpp" "CMakeFiles/BookStore.dir/Book.cpp.obj" "msvc" "CMakeFiles/BookStore.dir/Book.cpp.obj.d"
+  "C:/Users/User/OneDrive/Documents/School/CSCI 271-3024/BookStore-main/BookSet.cpp" "CMakeFiles/BookStore.dir/BookSet.cpp.obj" "msvc" "CMakeFiles/BookStore.dir/BookSet.cpp.obj.d"
+  "C:/Users/User/OneDrive/Documents/School/CSCI 271-3024/BookStore-main/Magazine.cpp" "CMakeFiles/BookStore.dir/Magazine.cpp.obj" "msvc" "CMakeFiles/BookStore.dir/Magazine.cpp.obj.d"
+  "C:/Users/User/OneDrive/Documents/School/CSCI 271-3024/BookStore-main/PrintedMedia.cpp" "CMakeFiles/BookStore.dir/PrintedMedia.cpp.obj" "msvc" "CMakeFiles/BookStore.dir/PrintedMedia.cpp.obj.d"
+  "C:/Users/User/OneDrive/Documents/School/CSCI 271-3024/BookStore-main/main.cpp" "CMakeFiles/BookStore.dir/main.cpp.obj" "msvc" "CMakeFiles/BookStore.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/BookStore.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "BookStore.exe"
+  "BookStore.exe.manifest"
+  "BookStore.lib"
+  "BookStore.pdb"
+  "CMakeFiles/BookStore.dir/Book.cpp.obj"
+  "CMakeFiles/BookStore.dir/Book.cpp.obj.d"
+  "CMakeFiles/BookStore.dir/BookSet.cpp.obj"
+  "CMakeFiles/BookStore.dir/BookSet.cpp.obj.d"
+  "CMakeFiles/BookStore.dir/Magazine.cpp.obj"
+  "CMakeFiles/BookStore.dir/Magazine.cpp.obj.d"
+  "CMakeFiles/BookStore.dir/PrintedMedia.cpp.obj"
+  "CMakeFiles/BookStore.dir/PrintedMedia.cpp.obj.d"
+  "CMakeFiles/BookStore.dir/main.cpp.obj"
+  "CMakeFiles/BookStore.dir/main.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BookStore.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
